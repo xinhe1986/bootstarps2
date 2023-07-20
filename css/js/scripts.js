f8df50c05a1c67215f366fef7beb9377ac5273ea
Xin He <xinhe19861@gmail.com>
2023-07-19 21:47:45 -0400

    Week 4 Assignment

--- css/js/scripts.js
+++ css/js/scripts.js
@@ -1,15 +1,25 @@
+
 $(function () {
-    $(function () {
-        $(".carousel").carousel({ interval: 500 });
-        $("#carouselButton").click(function () {
-            if ($("#carouselButton").children("i").hasClass("fa-pause")) {
-                $(".carousel").carousel("pause");
-                $("#carouselButton").children("i").removeClass("fa-pause");
-                $("#carouselButton").children("i").addClass("fa-play");
-            } else {
-                $(".carousel").carousel("cycle");
-                $("#carouselButton").children("i").removeClass("fa-play");
-                $("#carouselButton").children("i").addClass("fa-pause");
-            }
-        });
+    $(".carousel").carousel({ interval: 2000 });
+    $("#carouselButton").click(function () {
+        if ($("#carouselButton").children("i").hasClass("fa-pause")) {
+            $(".carousel").carousel("pause");
+            $("#carouselButton").children("i").removeClass("fa-pause");
+            $("#carouselButton").children("i").addClass("fa-play");
+        } else {
+            $(".carousel").carousel("cycle");
+            $("#carouselButton").children("i").removeClass("fa-play");
+            $("#carouselButton").children("i").addClass("fa-pause");
+        }
     });
+});
+$(function () {
+    $("#reserveButton").click(function () {
+        $('#reserveModal').modal("show");
+    });
+});
+$(function () {
+    $("#loginButton").click(function () {
+        $('#loginModal').modal("show");
+    });
+});
